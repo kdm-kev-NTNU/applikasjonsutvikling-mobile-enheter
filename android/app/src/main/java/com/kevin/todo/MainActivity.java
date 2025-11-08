982a39e4e25d654893b405250c6ec16092d9d61e
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-11-08 10:55:29 +0100

    videre oppstartskonfigurasjon

---
+++ android/app/src/main/java/com/kevin/todo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kevin.todo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kevin.todo',
+  appName: 'Todo',
+	webDir: 'dist'
+};
+
+export default config;
