db0d042ca625135fb4d8a3170c0d8f55c66dbfda
Kevin Dennis Mazali <kevindm@stud.ntnu.no>
2025-11-09 11:54:21 +0100

    døpe applikasjonen med nanvet handlekurva

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>{title}</IonTitle>
+          <IonTitle>Handlekurva</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
